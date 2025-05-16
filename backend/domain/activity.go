45a00e73cf71c676d9dad0aaf60583edb9d7e651
NicolasBono <2318599@ucc.edu.ar>
2025-05-16 13:21:28 -0300

    Descripción clara del cambio

--- backend/domain/activity.go
+++ backend/domain/activity.go
@@ -1,1 +1,12 @@
 package domain
+
+type Activity struct {
+	Id          int    `gorm:"primary_key"`
+	Name        string `gorm:"type:varchar(350);not null"`
+	Description string `gorm:"type:varchar(350);not null"`
+	Capacity    int    `gorm:"not null"`
+	category    string `gorm:"type:varchar(350);not null"`
+	profesor    string `gorm:"type:varchar(350);not null"`
+	day         string `gorm:"type:varchar(350);not null"`
+	hour        string `gorm:"type:varchar(350);not null"`
+}
