4d8f4c278379731d45909b4c562fc2784ab3f73e
danteferrer <135678008+danteferrer@users.noreply.github.com>
2025-05-30 09:50:05 -0300

    Finaliza merge de origin/dev en dev

--- backend/controllers/enrollment/enrollmentController.go
+++ backend/controllers/enrollment/enrollmentController.go
@@ -1,8 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	services "gym-api/backend/services/enrollmentService"
+
+	"github.com/gin-gonic/gin"
+)
+
+type EnrollmentController struct {
+	EnrollmentService services.EnrollmentServiceInterface
+}
+type EnrollmentControllersInterface interface {
+	CreateEnrollment(ctx *gin.Context)
+	GetEnrollment(ctx *gin.Context)
+}
 
 func CreateEnrollment(ctx *gin.Context) {
+
 }
 
 func GetEnrollment(ctx *gin.Context) {
